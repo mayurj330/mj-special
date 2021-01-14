3927d3c37931ea66b5001da6b45ad8f66822c3e2
mayurj330 <76876187+mayurj330@users.noreply.github.com>
2021-01-14 22:30:58 +0530

    Add files via upload

---
+++ Section 7 IF ELIF ELSE.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+if True:
+    print ("get some slips")
+
+
+# In[5]:
+
+
+if False:
+    print ("get some slips")
+
+
+# In[6]:
+
+
+if 5>2:
+    print("get somw sleep")
+
+
+# In[ ]:
+
+
+if 1>2:
+    print("get some sleep")
+
+
+# In[ ]:
+
+
+tired = False
+if tired:
+    print("get some sleep")
+else:
+    print("not get sleep")
+
+
+# In[14]:
+
+
+tired = True
+if tired:
+    print("get somw sleep")
+else:
+    print("not get sleep")
+
+
+# In[15]:
+
+
+tired = input("Are you tired?True or False.")
+if tired:
+    print("get somw sleep")
+else:
+    print("not get sleep")
+
+
+# In[ ]:
+
+
+tired = input("Are you tired? True or False.")
+if tired:
+    print("get somw sleep")
+else:
+    print("not get sleep")
+
+
+# In[ ]:
+
+
+tired = input("Are you tired? True or False.")
+if tired:
+    print("get somw sleep")
+else:
+    print("not get sleep")
+
+
+# In[ ]:
+
+
+tired = input("Are you tired? True or False.")
+if tired in("True","true","t","T"):
+    print("get somw sleep")
+else:
+    print("not get sleep")
+
+
+# In[ ]:
+
+
+
+
